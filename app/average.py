8e3f868f92fc5dc5b5ed397b98daf039c9a60333
xabuk44 <xabuk44@gmail.com>
2019-02-26 23:56:27 +0300

    add func

--- app/average.py
+++ app/average.py
@@ -1,13 +1,59 @@
 def shop_sum(week_money):
     return sum(week_money)
 
-shop_1_sum = shop_sum([5000, 2000, 3000, 1000, 4000, 6000, 7000])
-shop_2_sum = shop_sum([50000, 20000, 30000, 10000, 40000, 60000, 70000])
-print(shop_1_sum)
-print(shop_2_sum)
+# def shop_avg(week_money):
+#     import numpy
+#     return numpy.average(week_money)
+#
+# def shop_min(week_money):
+#     return min(week_money)
+#
+# def shop_max(week_money):
+#     return max(week_money)
+#
+# def shop_best3(week_money):
+#     week_money.sort()
+#     return week_money[4:7]
+#
+# shop_1_sum = shop_sum([5000, 2000, 3000, 1000, 4000, 6000, 7000])
+# shop_2_sum = shop_sum([5000, 2000, 3000, 1000, 4000, 6000, 7000])
+# shop_sum_list = [shop_1_sum, shop_2_sum]
+# print(shop_sum_list)
+#
+# shop_1_avg = shop_avg([5000, 2000, 3000, 1000, 4000, 6000, 7000])
+# print(shop_1_avg)
+# shop_1_min = shop_min([5000, 2000, 3000, 1000, 4000, 6000, 7000])
+# print(shop_1_min)
+# shop_1_max = shop_max([5000, 2000, 3000, 1000, 4000, 6000, 7000])
+# print(shop_1_max)
+# shop_1_best3 = shop_best3([5000, 2000, 3000, 1000, 4000, 6000, 7000])
+# print(shop_1_best3)
+#
+# shop_1 = [5000, 2000, 3000, 1000, 4000, 6000, 7000]
+# shop_2 = [4000, 2000, 3000, 1000, 4000, 6000, 7000]
 
-def shop_avg(week_money):
+# shop_1_sum = shop_sum([5000, 2000])
+# shop_2_sum = shop_sum([2000, 5000])
+# shop_3_sum = shop_sum([1000, 2000])
+# shop_sum_all = [shop_1_sum, shop_2_sum, shop_3_sum]
+# max_sum = max(shop_sum_all)
+# print(max_sum)
+
+def shop_max(shop_sum):
+    for objname,oid in globals().items():
+        if oid is shop_sum:
+            return objname
+
+def shop_money(week_money):
+    a = sum(week_money)
+    week_money.sort()
     import numpy
-    return numpy.average(week_money)
+    b = numpy.average(week_money)
+    return a, b, min(week_money), max(week_money), week_money[4:7]
 
-shop_1_avg =
+shop_1_sum = sum([5000, 2000])
+shop_2_sum = sum([5000, 2000])
+shop_3_sum = sum([1000, 2000])
+shop_sum_all = [shop_1_sum, shop_2_sum, shop_3_sum]
+
+print(shop_max(max(shop_sum_all)))
--- app/main_shop.py
+++ app/main_shop.py
@@ -5,12 +5,30 @@ def shop_money(week_money):
     b = numpy.average(week_money)
     return a, b, min(week_money), max(week_money), week_money[4:7]
 
-shop_1 = shop_money([5000, 2000, 3000, 1000, 4000, 6000, 7000])
-shop_2 = shop_money([50000, 20000, 30000, 10000, 40000, 60000, 70000])
-print(shop_1)
-print(shop_2)
 
-# import numpy
-# a = (1, 2, 3, 5, 6)
-# b = numpy.average(a)
-# print(b)
+shop_1 = shop_money([6005, 2000, 3000, 1000, 4000, 6000, 7000])
+shop_2 = shop_money([4000, 2000, 3000, 1000, 4000, 6000, 7000])
+shop_3 = shop_money([3000, 2000, 3000, 1000, 4000, 6000, 7000])
+
+
+
+best_sum_shop = [shop_1[0:1], shop_2[0:1], shop_3[0:1]]
+best_sum_shop = max(best_sum_shop)
+best_sum_shop = ' '.join([str(i) for i in best_sum_shop])
+print(best_sum_shop)
+
+best_avg_shop = [shop_1[1:2], shop_2[1:2], shop_3[1:2]]
+best_avg_shop = (max(best_avg_shop))
+best_avg_shop = ' '.join([str(i) for i in best_avg_shop])
+best_avg_shop = float(best_avg_shop)
+best_avg_shop = int(best_avg_shop)
+print(best_avg_shop)
+
+
+
+# shop_1_sum = [5000, 2000, 3000, 1000, 4000, 6000, 7000]
+# shop_2_sum = [4000, 2000, 3000, 1000, 4000, 6000, 7000]
+# summa = [sum(shop_1_sum), sum(shop_2_sum)]
+# summa.sort()
+# print(summa)
+
